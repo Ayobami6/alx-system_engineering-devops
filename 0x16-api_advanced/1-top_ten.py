e5c4d7e0f3dd3ebb19516854d3ea0865423d66ae
Ayobami <ayobamidele006@gmail.com>
2023-06-06 18:53:49 +0100

    practice fetching&parsing data from reddit api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usrbin/python3
+""" Get top ten posts """
+
+import requests
+
+
+def top_ten(subreddit):
+    """ Get top ten posts
+
+    Args:
+        subreddit (str): subreddit name
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Ayo User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json().get('data').get('children')  # list of posts
+    for post in posts[:10]:
+        print(post.get('data').get('title'))
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Get all posts recursively """
+
+import requests
+from typing import List
+from typing import Optional
+
+
+def recurse(subreddit: str, hot_list: Optional[List[str]] = None, after:
+            Optional[str] = None) -> List[str]:
+    """ Get all posts recursively
+
+    Args:
+        subreddit (str): subreddit name
+        hot_list (List[str]): list of hot posts
+        after (str): after post id
+
+    Returns:
+        List[str]: list of hot posts
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Ayo User Agent 1.0'}
+    params = {'limit': 100}
+    if after:
+        params['after'] = after
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    posts = response.json().get('data').get('children')  # list of posts
+    if hot_list is None:
+        hot_list = []
+    for post in posts:
+        hot_list.append(post.get('data').get('title'))
+    after = response.json().get('data').get('after')
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
